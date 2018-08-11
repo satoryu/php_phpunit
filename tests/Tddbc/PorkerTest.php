9e7efda336665197153efc3859f24061fb6eea0c
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2018-08-11 18:58:22 +0900

    Define a TestCase class for Porker

---
+++ tests/Tddbc/PorkerTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Porker;
+
+use PHPUnit\Framework\TestCase;
+
+class PorkerTest extends TestCase
+{
+    /**
+    * @test
+    */
+    public function isOnePair()
+    {
+        $this->assertEquals(true, true);
+    }
+}
