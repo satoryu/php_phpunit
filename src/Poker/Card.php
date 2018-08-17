032f3a0b4435d3e1a7ca2d1c3369555e9ad4d0cc
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2018-08-17 19:06:32 +0900

    Implement a tiny implement for Card

---
+++ src/Poker/Card.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Poker;
+
+class Card
+{
+    private $suit;
+    private $rank;
+
+    public function __construct($suit, $rank)
+    {
+        $this->suit = $suit;
+        $this->rank = $rank;
+    }
+
+    public function getSuit()
+    {
+        return $this->suit;
+    }
+}
---
+++ tests/Poker/CardTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Poker;
+
+use PHPUnit\Framework\TestCase;
+
+use Poker\Card;
+
+class CardTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function constructor()
+    {
+        $card = new Card('Heart', 1);
+
+        $this->assertEquals('Heart', $card->getSuit());
+    }
+}
